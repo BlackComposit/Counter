c7356863f54acbb35cef170e84ee35888c4e951d
BlackComposit <blackfianit@yandex.ru>
2023-10-26 00:50:15 +0300

    Добавлен лейбл счётчика и кнопки его увеличения, уменьшения и обновления. Добавлены действия кнопок .

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -9,11 +9,30 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var counterLabel: UILabel!
+    @IBOutlet weak var plusButton: UIButton!
+    @IBOutlet weak var minusButton: UIButton!
+    @IBOutlet weak var refreshButton: UIButton!
+    
+    private var counterValue: Int = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    @IBAction func plusButtonDidTap() {
+        counterValue += 1
+        counterLabel.text = "\(counterValue)"
+    }
+    @IBAction func minusButtonDidTap(_ sender: Any) {
+        guard counterValue > 0 else { return }
+        counterValue -= 1
+        counterLabel.text = "\(counterValue)"
+    }
+    @IBAction func refrashButtonDidTap() {
+        counterValue = 0
+        counterLabel.text = "\(counterValue)"
+    }
+    
 }
 
