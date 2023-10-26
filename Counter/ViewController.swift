04a50a9754dc866ee29d9fd2dffb4d0b0cbf22a6
BlackComposit <blackfianit@yandex.ru>
2023-10-27 00:05:59 +0300

    Добавлена история изменений счётчика. Добавлена функция получения текущего времени.

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -13,26 +13,48 @@ class ViewController: UIViewController {
     @IBOutlet weak var plusButton: UIButton!
     @IBOutlet weak var minusButton: UIButton!
     @IBOutlet weak var refreshButton: UIButton!
+    @IBOutlet weak var changeHistoryTextView: UITextView!
     
     private var counterValue: Int = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
-    @IBAction func plusButtonDidTap() {
-        counterValue += 1
-        counterLabel.text = "\(counterValue)"
-    }
-    @IBAction func minusButtonDidTap(_ sender: Any) {
-        guard counterValue > 0 else { return }
-        counterValue -= 1
-        counterLabel.text = "\(counterValue)"
-    }
-    @IBAction func refrashButtonDidTap() {
-        counterValue = 0
-        counterLabel.text = "\(counterValue)"
+        changeHistoryTextView.text = "История изменений:\n"
     }
     
+    @IBAction func plusButtonDidTap() {
+        let result = getCurrentDate()
+        counterValue += 1
+        counterLabel.text = "\(counterValue)"
+        changeHistoryTextView.text += "\(result) значение изменено на +1\n"
+    }
+    
+    @IBAction func minusButtonDidTap() {
+        guard counterValue > 0 else {
+            let result = getCurrentDate()
+            changeHistoryTextView.text += "\(result) попытка уменьшить значение счётчика ниже 0\n"
+            return
+        }
+        let result = getCurrentDate()
+        counterValue -= 1
+        counterLabel.text = "\(counterValue)"
+        changeHistoryTextView.text += "\(result) значение изменено на -1\n"
+    }
+    
+    @IBAction func refreshButtonDidTap() {
+        let result = getCurrentDate()
+        counterValue = 0
+        counterLabel.text = "\(counterValue)"
+        changeHistoryTextView.text += "\(result) значение сброшено\n"
+    }
+    
+    private func getCurrentDate() -> String {
+        let date = Date()
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateStyle = .long
+        dateFormatter.timeStyle = .medium
+        dateFormatter.locale = Locale(identifier: "ru_RU")
+        return dateFormatter.string(from: date)
+    }
 }
 
